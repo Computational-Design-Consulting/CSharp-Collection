af3ea78cb4de964287e34df962fcbb5c87f2968e
timsdesigns <tims.designs@yahoo.com>
2022-02-08 19:33:28 +0100

    Adding Async Methods
     - building async workflow

--- Own examples and Ideas/EventMailEx/EventMailExLibrary/EventPublisherA.cs
+++ Own examples and Ideas/EventMailEx/EventMailExLibrary/EventPublisherA.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace EventMailExLibrary
 {
@@ -21,15 +22,21 @@ namespace EventMailExLibrary
     /// <param name="messageFromPublisher">expecting string input</param>
     public delegate void Event1Handler(string messageFromPublisher);      // ○ Declare Delegate     
 
+    public delegate Task AsyncEvent1Handler(string messageFromPublisher);
+
     /// <summary>
     /// Calls matching method on Publisher instance in subscriber
     /// </summary>
     public event Event1Handler Event1;                                    // ○ Declare Event of type Delegate, raised when called
 
+    public event AsyncEvent1Handler AsyncEvent1;
+
     /// <summary>
     /// Method used on instance to trigger event and pass method-string-parameter
     /// </summary>
     public void RaiseEvent1() => Event1($"\"raised {++counter} times\""); // ○ Raise(Call) event, incrementing event-occurance-counter within method
+
+    public async Task RasiseAsyncEvent1Async() => await AsyncEvent1($"\"raised {++counter} times\""); 
 
     /// <summary>
     /// passing Library Assembly to Program, because Library holds embeded ressource
