ec58139e25fb8a917cfb6e1718d11d1c4d8143f9
timsdesigns <tims.designs@yahoo.com>
2022-02-08 19:31:01 +0100

    Update Program.cs
     - rewriting using .Net Core

--- Own examples and Ideas/EventMailEx/EventMailEx/Program.cs
+++ Own examples and Ideas/EventMailEx/EventMailEx/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Mail;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using EventMailExLibrary;
 
 namespace EventMailEx
@@ -37,20 +40,21 @@ namespace EventMailEx
       "\"Do you want to send a notification by mail? (y/else)\"\n" +
       "has been answered with \"y\", an email is sent to the following test-account:\n\n";*/
     #endregion
-    
-    
+
+
     /// <summary>
     /// Entry point, subscribing & raising event
     /// </summary>
     /// <param name="args"></param>
-    static void Main(string[] args)                                           /// Program Entry Point
+    //static Task Main(string[] args)                                           /// Program Entry Point
+    static async Task Main(string[] args)                                           /// Program Entry Point
     {
-      MailMessage message;
-      SmtpClient client;
-      SendingMail.MailSetup(out message, out client);
-      Console.WriteLine("here"+client.Credentials);
-
-      Console.WriteLine(SendingMail.GetIndexHTML());
+      //Preparing test - working way to print embeded HTML-Mail:
+      //MailMessage message;
+      //SmtpClient client;
+      //SendingMail.MailSetup(out message, out client);
+      //Console.WriteLine("here" + client.Credentials);
+      //Console.WriteLine(SendingMail.GetIndexHTML());
 
       //Console.WriteLine(_userInstructions+_addressEmail);                                   /// - user instructions long version
       Console.WriteLine(_userInstructionsShort + _addressEmail);                              /// - user instructions short version
@@ -64,16 +68,25 @@ namespace EventMailEx
       ///   Binding (Publisher-instance.)Event:Event1
       ///   > with (Publisher-type.)delegate:Event1Handler
       ///   > calling argument-method:OnEvent1_GiveConsoleFeedback()
-      subscriberEventObject.Event1 += new EventPublisherA.Event1Handler(OnEvent1_GiveConsoleFeedback);
-      subscriberEventObject.Event1 += new EventPublisherA.Event1Handler(OnEvent1_SendMail);
+      //subscriberEventObject.Event1 +=
+      //  new EventPublisherA.Event1Handler(OnEvent1_GiveConsoleFeedback);
+      //subscriberEventObject.Event1 +=
+      //  new EventPublisherA.Event1Handler(OnEvent1_SendMail); 
+      subscriberEventObject.AsyncEvent1 +=
+        new EventPublisherA.AsyncEvent1Handler(OnAsyncEvent1_GiveConsoleFeedback);
+      subscriberEventObject.AsyncEvent1 +=
+        new EventPublisherA.AsyncEvent1Handler(OnAsyncEvent1_SendMail);
 
       do
       {
         /// ○ call publisher-class-method on instance
         /// --> fires event(which calls the delegate(that uses the event argument))
-        subscriberEventObject.RaiseEvent1();
+        //subscriberEventObject.RaiseEvent1();
 
-        Console.WriteLine("Repeat? (y/else)");                                  ///cosmetics
+
+        await Task.WhenAll(subscriberEventObject.RasiseAsyncEvent1Async());
+        // TODO: get following line to be executed after, or before, but not inbetween
+        Console.WriteLine("\nUser Prompt: Repeat? (y/else)");                                  ///cosmetics
       } while (Console.ReadLine().ToUpper().Contains('Y'));                     ///repeat execution  
     }
 
@@ -86,6 +99,80 @@ namespace EventMailEx
     {
       // ○ Code to be executed when Event1 fires.
       Console.WriteLine("event fired with argument: " + eventString);
+    }
+
+    /// <summary>
+    /// Method 1 Async called by delegate if event is raised (matching delegate)
+    /// </summary>
+    /// <param name="someString"></param>
+    public static async Task OnAsyncEvent1_GiveConsoleFeedback(string eventString /* - "raised x times"*/)
+    {
+      // Adding (redundant) Progress Bar fun using async:
+      var tasks = new List<Task<Action>>();
+      Console.Write("Start Feedback about OnAsyncEvent1_GiveConsoleFeedback progress: \n|{");
+      tasks.Add((Task<Action>)Task.Run(async ()=>
+      await Task.Run(
+        async () =>
+        {
+          int barLength = 20, mSec = 300; //barLength and stepTime
+
+          char done = '▓', ahead = '░', close = '}';
+          var progress = new StringBuilder(barLength);
+          var delete = new StringBuilder(barLength);
+          int relStepSize = mSec / barLength, counter = 0;
+          for (int i = 0; i < mSec; i++)
+            if (i % relStepSize <= 0)
+            {
+              await Task.Delay(mSec);   // Delay function to see bar growing, here so it's stepwise
+              int toGo = barLength - 1 - counter;
+              if (counter == 0)
+              {
+                progress.Append(done);
+                for (int j = 0; j < toGo; j++)  //construct strings according barLength
+                {
+                  delete.Append("\b");    // fill delete string
+                  progress.Append(ahead);   // fill relative left bar length
+                }
+                delete.Append("\b");    // fill delete string
+                progress.Append(close);
+              }
+              else
+              {
+                if (counter == 2)
+                  delete.Append("\b");
+                progress.Replace(ahead, done, counter, 1);
+                Console.Write(delete); //delete chars
+              }
+              Console.Write(progress); //write 
+              counter++;
+            }
+        })));
+      //await Task.WhenAll(tasks);
+
+      Console.WriteLine("'|' > tedious task done! End Feedback! \n\n");
+      Console.WriteLine("event fired with argument: " + eventString);
+    }
+
+
+    /// <summary>
+    /// Method 2 called by delegate if event is raised (matching delegate)
+    /// </summary>
+    /// <param name="eventString"></param>
+    public static async Task OnAsyncEvent1_SendMail(string eventString)
+    {
+      // ○ trigger email if event is raised more than 10 times:
+      // - retrieving integer-part from 
+      await Task.Run(async () =>
+      {
+        if (Convert.ToInt32(Regex.Match(eventString, @"\d+").Value) == 10)
+          if (++countEmails < 4)
+          {
+            Console.WriteLine("The event has been raised 10 times" +
+              "\nDo you want to send a notification by mail? (y/else)");
+            if (Console.ReadLine().ToUpper().Contains('Y'))
+              await SendingMail.CreateAndSendMessageWithAttachmentAsync();
+          }
+      });
     }
 
     /// <summary>
@@ -105,6 +192,5 @@ namespace EventMailEx
             SendingMail.CreateAndSendMessageWithAttachment();
         }
     }
-
   }
 }
